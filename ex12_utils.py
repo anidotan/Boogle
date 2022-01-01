e5cbffc044bfe34545573f03663448d51e9392b2
zukarbell <zukarbell@gmail.com>
2022-01-01 18:47:28 +0200

    first commit

---
+++ ex12_utils.py
@@ -0,0 +1,16 @@
+def is_valid_path(board, path, words):
+    word_according
+    for cell_tuple in path:
+        row, col = cell_tuple
+        =board[row][col]
+
+    pass
+
+def find_length_n_paths(n, board, words):
+    pass
+
+def find_length_n_words(n, board, words):
+    pass
+
+def max_score_paths(board, words):
+    pass
